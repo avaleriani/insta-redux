bb93cb100292d4b7baaf7ddec49854225d61ee50
avaleriani <avaleriani@digbang.com>
2016-06-29 15:04:14 -0300

    vid 6

---
+++ client/store.js
@@ -0,0 +1,21 @@
+import { createStore, compose } from 'react';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create object for default data
+const defaultState = {
+    posts,
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
